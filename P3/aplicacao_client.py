85277a9a437d21494760672f61ffca5186ab0028
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-09-02 14:42:08 -0300

    p3 start

---
+++ P3/aplicacao_client.py
@@ -0,0 +1,97 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2020
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+
+from enlace import *
+import time
+import numpy as np
+import os
+import io
+import PIL.Image as Image
+from array import array
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports;
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+serialName = "/dev/cu.usbmodem14101" # Mac    (variacao de)
+#serialName = "COM6"                  # Windows(variacao de)
+
+def img_to_bytes(img):
+
+    with open(img, "rb") as image:
+        f = image.read()
+        b = bytearray(f)
+        return b
+
+def bytes_to_img(bytes):
+    image = Image.open(io.BytesIO(bytes))
+    image.save('bytes.jpeg')
+
+
+
+def main():
+    try:
+        com1 = enlace(serialName)
+        
+        com1.enable()
+        # txBuffer = random_command_list()
+        # com1.sendData(np.asarray(txBuffer))
+
+        com1.sendData(b'\xcc\xcc')
+        print("Sacrifice bytes sent...")
+        print("receiving confirmation...")
+        rxBuffer, nRx = com1.getData(2)
+        print("received!")
+        com1.rx.clearBuffer()
+
+        txBuffer = img_to_bytes("img.jpeg")
+        com1.sendData(np.asarray(txBuffer))
+        print("Data sent...")
+        print(txBuffer)
+
+        txSize = com1.tx.getStatus()
+
+        receive = True
+        received_bytes_list = []
+
+        while receive:
+            rxBuffer, nRx = com1.getData(1)
+            if rxBuffer ==  b'\xBB':
+                receive = False
+            elif rxBuffer == b'\xAA':
+                rxBuffer, nRx = com1.getData(2)
+                received_bytes_list.append(rxBuffer)
+            else:
+                received_bytes_list.append(rxBuffer)
+        print(received_bytes_list)
+
+
+
+            
+    
+        # Encerra comunicação
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com1.disable()
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
