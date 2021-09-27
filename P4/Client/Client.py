662697bb5ede3bec794b73da69a16af687c1951d
pauloschade <paulosc4@al.insper.edu.br>
2021-09-27 17:17:05 -0300

    starting logs

--- P4/Client/Client.py
+++ P4/Client/Client.py
@@ -13,9 +13,11 @@ class Client():
 
         self.sending_type = 1
         self.type = 0
-        self.error = False
+        self.error = True
         self.timer2 = 0
         self.timeout = False
+
+        self.transmission = 1
 
     def count_timer2(self):
         if self.timer2 > 3:
@@ -31,26 +33,29 @@ class Client():
         self.send_packages()
 
     def set_start(self):
-        self.start_package =  utils.make_start_package(self.total_packages, self.sending_type)
+        self.start_package =  utils.make_start_package(self.total_packages, 1)
 
     def setup_packages(self):
-        self.sending_type = 3
-        self.packages = utils.make_packages(self.buffer, self.sending_type)
+        self.packages = utils.make_packages(self.buffer, 3)
         self.total_packages = len(self.packages)
-        self.sending_type = 1
         self.set_start()
 
     def send_start_package(self):
+        self.sending_type = 1
         self.com1.sendData(self.start_package)
+        self.log("envio")
         time.sleep(1)
 
     def send_packages(self):
+        self.sending_type = 3
         if self.error and self.index == 7:
             self.com1.sendData(self.packages[self.index + 4] +  b'\xFF\xAA\xFF\xAB') 
             self.error = False
+            self.transmission = 2
         else:
             self.packages[self.index] +=  b'\xFF\xAA\xFF\xAA'
             self.com1.sendData(self.packages[self.index])
+        self.log("envio")
         self.index+=1
         if self.index == self.total_packages:
             self.done = True
@@ -63,6 +68,7 @@ class Client():
                 self.timeout = True
                 self.count_timer2()
                 self.set_timeout()
+                self.transmission = 5
                 break
             else:
                 self.timeout = False
@@ -76,6 +82,7 @@ class Client():
                     assert rx_int == self.id, "Not for me"
 
                 if i == 6 and self.type == 6:
+                    self.sending_type = 6
                     print("Resend package, received error type 6")
                     
                     self.index = rx_int - 1
@@ -91,12 +98,26 @@ class Client():
 
     
     def receive_package(self):
+        self.sending_type = 4
         self.read_head()
         if not self.timeout:
             self.read_eoc()
             self.com1.rx.clearBuffer()
         else:
             pass
+        self.log("recibo")
+
+    def log(self, envio):
+        if self.sending_type == 3:
+            s = [envio, self.sending_type, len(self.packages[self.index-1]), self.index + 1, self.total_packages]
+        else:
+            s = [envio, self.sending_type, 14]
+        with open(f"Client{self.transmission}.txt", "a") as f:
+            for i in s:
+                f.write(str(i) + " / ")
+            f.write("\n")
+
+
 
     
 
